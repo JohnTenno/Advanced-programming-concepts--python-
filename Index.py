8ad01072a4a8fb1138bfea0f250281fca151d207
JohnTenno <131226056+JohnTenno@users.noreply.github.com>
2023-09-12 14:42:53 -0600

    index

---
+++ Index.py
@@ -0,0 +1,11 @@
+# Proyecto de Conceptos Avanzados de programacion
+# Jonathan Gandara Salazar
+
+# Agreguen aqui sus nombres como comentario
+
+
+def main():
+    print("¡Hola mundo!")
+
+if __name__ == "__main__":
+    main()
