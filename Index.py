123067a20a0994acf9912b2409690367f0c3458e
JohnTenno <131226056+JohnTenno@users.noreply.github.com>
2023-09-13 16:47:00 -0600

    call funtion

--- Index.py
+++ Index.py
@@ -3,10 +3,11 @@
 # Carlos Arguello Maladonado
 
 # Agreguen aqui sus nombres como comentario
-
+import funcion1
 
 def main():
     print("¡Hola mundo!")
+    funcion1()
 
 if __name__ == "__main__":
     main()
