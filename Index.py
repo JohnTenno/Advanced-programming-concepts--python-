d0c6c241c32b13e569b625afe6365cd0f594d159
JohnTenno <131226056+JohnTenno@users.noreply.github.com>
2023-09-13 22:31:03 -0600

    menu

--- Index.py
+++ Index.py
@@ -3,11 +3,27 @@
 # Carlos Arguello Maladonado
 
 # Agreguen aqui sus nombres como comentario
-import funcion1
 
 def main():
-    print("¡Hola mundo!")
-    funcion1()
+    opcion = ""
+    while opcion != "3":
+        print("Menú:")
+        print("1. Calcular fórmula general")
+        print("2. Otra opción")
+        print("3. Salir")
+        
+        opcion = input("Ingrese el número de la opción que desea ejecutar: ")
+        
+        if opcion == "1":
+            from formulaGeneral.formula_general import resolver_ecuacion_cuadratica
+        elif opcion == "2":
+            # Agrega aquí la lógica para la opción 2
+            pass
+        elif opcion == "3":
+            print("Saliendo del programa...")
+        else:
+            print("Opción inválida. Por favor, ingrese un número válido.")
+        print()  # Agrega una línea en blanco después de cada opción seleccionada
 
 if __name__ == "__main__":
     main()
